3a6aa81265bf7b1ef72942764393cb56f1c16e18
wfc <244157602@qq.com>
2017-06-28 16:06:19 +0800

    添加文件

--- app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.administrator.jkbd;
+package com.example.administrator.jkbd.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.administrator.jkbd.R;
 
 public class MainActivity extends AppCompatActivity {
 
