dd1d8894a94376f10ddeee86322ec0948eef1e2f
weifucheng <244157602@qq.com>
2017-06-29 00:13:26 +0800

    闪屏主题调整

--- app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.administrator.jkbd.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.administrator.jkbd.R;
 
@@ -11,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void text(View view) {
+      //  Toast.makeText(this,"dd",Toast.LENGTH_LONG).show();
+        Intent intent=new Intent(MainActivity.this,QuestionActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/administrator/jkbd/activity/QuestionActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.jkbd.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.jkbd.R;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question_activity);
+    }
+}
