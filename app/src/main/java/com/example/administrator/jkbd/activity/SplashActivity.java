056110a37768cfaee68382176b8eabeda5c94bf3
weifucheng <244157602@qq.com>
2017-06-28 23:30:54 +0800

    添加闪屏功能

--- app/src/main/java/com/example/administrator/jkbd/activity/SplashActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/SplashActivity.java
@@ -1,10 +1,34 @@
 package com.example.administrator.jkbd.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.jkbd.R;
+
 /**
  * Created by Administrator on 2017/6/28.
  */
 
-public class SplashActivity {
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+        countDownTimer.start();
+    }
+    CountDownTimer countDownTimer=new CountDownTimer(2000,2000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
 
+        }
 
+        @Override
+        public void onFinish() {
+            Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
