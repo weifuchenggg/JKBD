4b504d6f85621f0fe709b11090078e05238ad3c0
wfc <244157602@qq.com>
2017-06-29 16:57:59 +0800

    接收数据

--- app/src/main/java/com/example/administrator/jkbd/activity/QuestionActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/QuestionActivity.java
@@ -1,9 +1,12 @@
 package com.example.administrator.jkbd.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.administrator.jkbd.R;
+import com.example.administrator.jkbd.bean.ExamInfo;
 
 public class QuestionActivity extends AppCompatActivity {
 
@@ -11,6 +14,8 @@ public class QuestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.question_activity);
-
+        Intent intent= new Intent();
+        ExamInfo examInfo=(ExamInfo) intent.getSerializableExtra("examInfo");
+        Log.e("main",examInfo+" 11111111111");
     }
 }
