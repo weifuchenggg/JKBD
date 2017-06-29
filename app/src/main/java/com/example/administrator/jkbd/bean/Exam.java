e22c5430d99e76f80a2367676ab25cc180ef0e5f
wfc <244157602@qq.com>
2017-06-29 09:51:17 +0800

    添加实体类

---
+++ app/src/main/java/com/example/administrator/jkbd/bean/Exam.java
@@ -0,0 +1,103 @@
+package com.example.administrator.jkbd.bean;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class Exam {
+
+
+    /**
+     * id : 19
+     * question : 这个标志是何含义？
+     * answer : 1
+     * item1 : 休息区
+     * item2 : 服务区
+     * item3 : 停车场
+     * item4 : 观景台
+     * explains : 这个是休息区，服务区是有加油，通车，吃饭的组合标志，停车场就是一个P，图中没有加油站和吃饭的，所以它只是一个休息区。
+     * url : http://images.juheapi.com/jztk/c1c2subject1/19.jpg
+     */
+
+    private int id;
+    private String question;
+    private String answer;
+    private String item1;
+    private String item2;
+    private String item3;
+    private String item4;
+    private String explains;
+    private String url;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getItem1() {
+        return item1;
+    }
+
+    public void setItem1(String item1) {
+        this.item1 = item1;
+    }
+
+    public String getItem2() {
+        return item2;
+    }
+
+    public void setItem2(String item2) {
+        this.item2 = item2;
+    }
+
+    public String getItem3() {
+        return item3;
+    }
+
+    public void setItem3(String item3) {
+        this.item3 = item3;
+    }
+
+    public String getItem4() {
+        return item4;
+    }
+
+    public void setItem4(String item4) {
+        this.item4 = item4;
+    }
+
+    public String getExplains() {
+        return explains;
+    }
+
+    public void setExplains(String explains) {
+        this.explains = explains;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/jkbd/bean/ExamInfo.java
@@ -0,0 +1,61 @@
+package com.example.administrator.jkbd.bean;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class ExamInfo {
+
+    /**
+     * subjectTitle : c1
+     * uid : 1
+     * limitTime : 10
+     * questionCount : 100
+     */
+
+    private String subjectTitle;
+    private int uid;
+    private int limitTime;
+    private int questionCount;
+
+    public String getSubjectTitle() {
+        return subjectTitle;
+    }
+
+    public void setSubjectTitle(String subjectTitle) {
+        this.subjectTitle = subjectTitle;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public int getLimitTime() {
+        return limitTime;
+    }
+
+    public void setLimitTime(int limitTime) {
+        this.limitTime = limitTime;
+    }
+
+    public int getQuestionCount() {
+        return questionCount;
+    }
+
+    public void setQuestionCount(int questionCount) {
+        this.questionCount = questionCount;
+    }
+
+    @Override
+    public String toString() {
+        return "ExamInfo{" +
+                "考试科目='" + subjectTitle + '\'' +
+                ", 时限=" + limitTime +
+                ", 考题数量=" + questionCount +
+                '}';
+    }
+}
