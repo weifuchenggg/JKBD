ef1a32d945799c0e7a66d78c894032f4b483f8d8
wfc <244157602@qq.com>
2017-06-30 11:05:54 +0800

    布局修改为单选按钮

--- app/src/main/java/com/example/administrator/jkbd/activity/QuestionActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/QuestionActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +18,8 @@ public class QuestionActivity extends AppCompatActivity {
 
     int k=0;
     TextView textView,tv_a,tv_b,tv_c,tv_d,tv_question;
+    RadioButton rb_c,rb_d;
+    RadioGroup rg;
     ExamInfo examInfo;
     Result result;
     @Override
@@ -35,8 +39,20 @@ public class QuestionActivity extends AppCompatActivity {
     private void update(){
         tv_a.setText("A."+result.getResult().get(k).getItem1());
         tv_b.setText("B."+result.getResult().get(k).getItem2());
-        tv_c.setText("C."+result.getResult().get(k).getItem3());
-        tv_d.setText("D."+result.getResult().get(k).getItem4());
+        if(result.getResult().get(k).getItem3()==null || result.getResult().get(k).getItem3().equals("")){
+            tv_c.setVisibility(View.GONE);
+            tv_d.setVisibility(View.GONE);
+            rb_c.setVisibility(View.INVISIBLE);
+            rb_d.setVisibility(View.INVISIBLE);
+        }else{
+            tv_c.setVisibility(View.VISIBLE);
+            tv_d.setVisibility(View.VISIBLE);
+            rb_c.setVisibility(View.VISIBLE);
+            rb_d.setVisibility(View.VISIBLE);
+            tv_c.setText("C."+result.getResult().get(k).getItem3());
+            tv_d.setText("D."+result.getResult().get(k).getItem4());
+        }
+        rg.clearCheck();
         tv_question.setText((k+1)+"."+result.getResult().get(k).getQuestion());
 
     }
@@ -47,16 +63,27 @@ public class QuestionActivity extends AppCompatActivity {
         tv_b=(TextView) findViewById(R.id.tv_b);
         tv_c=(TextView) findViewById(R.id.tv_c);
         tv_d=(TextView) findViewById(R.id.tv_d);
+        rb_c=(RadioButton)findViewById(R.id.rb_c);
+        rb_d=(RadioButton)findViewById(R.id.rb_d);
+        rg=(RadioGroup)findViewById(R.id.rg);
         tv_question=(TextView) findViewById(R.id.tv_question);
     }
 
     public void next(View view) {
-        if(k<99){
-            k++;
-            update();
-        }else{
-            Toast.makeText(this,"已经是最后一题",Toast.LENGTH_LONG).show();
+        if(k<=99){
+            if(rg.getCheckedRadioButtonId()==-1){
+                Toast.makeText(this,"答案未选择",Toast.LENGTH_SHORT).show();
+                return ;
+            }
         }
+        if(k==99){
+
+            Toast.makeText(this,"已经是最后一题,请交卷",Toast.LENGTH_LONG).show();
+            return ;
+        }
+        k++;
+        update();
+
     }
 
     public void prev(View view) {
